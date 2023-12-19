e8a67e2171baa5e5c3cbf7aa8ec0d6afa80f71bc
Akhil Gireesh <akhilgireeshakg@gmail.com>
2023-12-19 06:20:03 +0530

    footer

--- frontend/src/components/footer.jsx
+++ frontend/src/components/footer.jsx
@@ -6,14 +6,17 @@ import gm from "../assets/mail.png"
 const Footer = () => {
     return(
         <div className="footerContainer">
+            <div className="footerBeASponsor">
+                
+            </div>
             <div className="footerBoxContainer">
                 <div className="footer-Box" id="fb1">
                     Logo
                 </div>
                 <div className="footer-Box" id="fb2">
-                    <img src={fb} alt="FB"></img>
-                    <img src={ig} alt="IG"></img>
-                    <img src={gm} alt="Mail"></img>
+                    <img src={fb} alt="FB" className="fb2-icons"></img>
+                    <img src={ig} alt="IG" className="fb2-icons"></img>
+                    <img src={gm} alt="Mail" className="fb2-icons"></img>
                 </div>
                 <div className="footer-Box" id="fb3">
                     Our Sponsors
