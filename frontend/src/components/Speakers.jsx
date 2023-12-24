e3f15c15a19c2f59d522b07170db6b90d0171907
AKHIL-GIREESH <94656684+AKHIL-GIREESH@users.noreply.github.com>
2023-12-25 03:50:20 +0530

    Revert back Speakers

--- frontend/src/components/Speakers.jsx
+++ frontend/src/components/Speakers.jsx
@@ -11,7 +11,6 @@ const Speakers = () => {
     console.log(active)
 
     let speakerList = []
-   
     for(let i=1;i<5;i++){
         speakerList.push(
             <a href="#SpeakerInfo">
@@ -24,18 +23,14 @@ const Speakers = () => {
         )
     }
 
-    return( 
-    <>
-        <div className="flex flex-col lg:flex-row text-white min-h-screen items-center justify-center">
-        <div className="md:w-3/4  mt-5 md:mt-32 lg:mr-8 px-3 py-3"><div className="Speakers">
-            {/* <div className="footerBeASponsor"> */}
+    return <div className="Speakers">
+            <div className="footerBeASponsor" style={{top:"-30vh",position:"sticky",border:"none",background: 'radial-gradient(#187747 70%,rgba(0, 0, 0, 0))',zIndex:""}}>
 
-            {/* </div> */}
-            
-            <div aria-label="Breadcrumb" className=" md:flex md:self-start hidden">
-                <ol className="flex items-center gap-1 text-sm bg-inherit border-0 text-white font-bold">
-                  <li>
-                    <a
+            </div>
+                <div aria-label="Breadcrumb" className=" md:flex md:self-start hidden" style={{ top: 0, position: 'sticky',zIndex:"2" }}>
+                 <ol className="flex items-center gap-1 text-sm bg-inherit border-0 text-white font-bold">
+                   <li>
+                   <a
                       href="/"
                       className="block transition hover:text-green-400"
                     >
@@ -86,61 +81,173 @@ const Speakers = () => {
                     </a>
                   </li>
                 </ol>
-              </div>        
-              
-              
-              
-              
-                  <div className="speakerCards">
+              </div> 
+            {/* <p><Link to="/"><span>Home</span></Link> -- Speakers</p> */}
+            <div className="speakerCards">
                 {speakerList}
             </div>
-            { <div className="flex flex-col lg:flex-row w-full mt-4">
-            {/* Speaker image */}
-            <div className="w-full lg:w-1/4 mb-4 lg:mb-0">
-              <div className="block rounded-lg p-4">
-                <img
-                  alt="Speaker"
-                  src="https://images.unsplash.com/photo-1664575602554-2087b04935a5?q=80&w=3087&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-                  className="h-56 w-full rounded-md object-cover"
-                />
-
-                <div className="mt-2">
-                  <dl>
-                    <div>
-                      <dd className="font-medium">Marine Gwenddydd</dd>
-                      <div>
-                        <dd className="text-sm text-gray-500">
-                          Managing Director
-                        </dd>
-                      </div>
-                    </div>
-                  </dl>
-                </div>
-              </div>
+            {active && <div id="SpeakerInfo" style={{flexWrap: window.innerHeight > window.innerWidth && "wrap"}}>
+            <div className="speakerCard">
+                <img src={AG}></img>
+                <span>Anantha Krishnan {active}</span>
+                <p>Student, Doctor, Astronaut, Dev</p>
             </div>
+            <div className="speaker-Description" style={{height: window.innerHeight > window.innerWidth  && '70vh'}}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div>
+            </div>}
 
-            {/* Speaker description */}
-            <div className="w-full lg:w-3/4 lg:ml-4 h-56 rounded-lg box-border border-gray-800 border flex items-center px-5">
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quas
-              optio delectus velit earum qui, dicta excepturi voluptas beatae
-              tenetur inventore harum recusandae, placeat minus dignissimos
-              doloribus quo voluptate debitis consectetur vel reprehenderit
-              molestiae. Optio omnis quis, ex, laudantium reiciendis eveniet
-              officiis maiores aut error earum mollitia at perspiciatis sint
-              soluta.
-            </div>
-          </div>}
-
-</div>
-</div>
-
-       
-        </div>
+        
         <Footer/>
-        </>)
+        </div>
 }
 
 export default Speakers
+
+
+
+
+// import Footer from "./footer"
+// import AK from "../assets/ak.jpg"
+// import AG from "../assets/AG.jpeg"
+// import { useState } from "react"
+// import { Link } from "react-router-dom";
+
+// const Speakers = () => {
+
+//     const [active,SetActive] = useState(false);
+
+//     console.log(active)
+
+//     let speakerList = []
+   
+//     for(let i=1;i<5;i++){
+//         speakerList.push(
+//             <a href="#SpeakerInfo">
+//             <div className="speakerCard" style={{border: active === i && '2px solid #33FFA9'}} key={i} onClick={() => SetActive(i)}>
+//                 <img src={AK}></img>
+//                 <span>Anantha Krishnan {i}</span>
+//                 <p>Student, Doctor, Astronaut, Dev</p>
+//             </div>
+//             </a>
+//         )
+//     }
+
+//     return( 
+//     <>
+//         <div className="flex flex-col lg:flex-row text-white min-h-screen items-center justify-center">
+//         <div className="md:w-3/4  mt-5 md:mt-32 lg:mr-8 px-3 py-3"><div className="Speakers">
+//             {/* <div className="footerBeASponsor"> */}
+
+//             {/* </div> */}
+            
+//             <div aria-label="Breadcrumb" className=" md:flex md:self-start hidden">
+//                 <ol className="flex items-center gap-1 text-sm bg-inherit border-0 text-white font-bold">
+//                   <li>
+//                     <a
+//                       href="/"
+//                       className="block transition hover:text-green-400"
+//                     >
+//                       <span className="sr-only"><Link to="/"> Home</Link> </span>
+
+//                       <svg
+//                         xmlns="http://www.w3.org/2000/svg"
+//                         className="h-4 w-4"
+//                         fill="none"
+//                         viewBox="0 0 24 24"
+//                         stroke="currentColor"
+//                       >
+//                         <path
+//                           stroke-linecap="round"
+//                           stroke-linejoin="round"
+//                           stroke-width="2"
+//                           d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"
+//                         />
+//                       </svg>
+//                     </a>
+//                   </li>
+
+             
+
+             
+
+//                   <li className="rtl:rotate-180">
+//                     <svg
+//                       xmlns="http://www.w3.org/2000/svg"
+//                       className="h-4 w-4"
+//                       viewBox="0 0 20 20"
+//                       fill="currentColor"
+//                     >
+//                       <path
+//                         fill-rule="evenodd"
+//                         d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
+//                         clip-rule="evenodd"
+//                       />
+//                     </svg>
+//                   </li>
+
+//                   <li>
+//                     <a
+//                       href="#"
+//                       className="block transition text-green-400 hover:text-green-400"
+//                     >
+//                       Speakers
+//                     </a>
+//                   </li>
+//                 </ol>
+//               </div>        
+              
+              
+              
+              
+//                   <div className="speakerCards">
+//                 {speakerList}
+//             </div>
+//             { <div className="flex flex-col lg:flex-row w-full mt-4">
+//             {/* Speaker image */}
+//             <div className="w-full lg:w-1/4 mb-4 lg:mb-0">
+//               <div className="block rounded-lg p-4">
+//                 <img
+//                   alt="Speaker"
+//                   src="https://images.unsplash.com/photo-1664575602554-2087b04935a5?q=80&w=3087&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+//                   className="h-56 w-full rounded-md object-cover"
+//                 />
+
+//                 <div className="mt-2">
+//                   <dl>
+//                     <div>
+//                       <dd className="font-medium">Marine Gwenddydd</dd>
+//                       <div>
+//                         <dd className="text-sm text-gray-500">
+//                           Managing Director
+//                         </dd>
+//                       </div>
+//                     </div>
+//                   </dl>
+//                 </div>
+//               </div>
+//             </div>
+
+//             {/* Speaker description */}
+//             <div className="w-full lg:w-3/4 lg:ml-4 h-56 rounded-lg box-border border-gray-800 border flex items-center px-5">
+//               Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quas
+//               optio delectus velit earum qui, dicta excepturi voluptas beatae
+//               tenetur inventore harum recusandae, placeat minus dignissimos
+//               doloribus quo voluptate debitis consectetur vel reprehenderit
+//               molestiae. Optio omnis quis, ex, laudantium reiciendis eveniet
+//               officiis maiores aut error earum mollitia at perspiciatis sint
+//               soluta.
+//             </div>
+//           </div>}
+
+// </div>
+// </div>
+
+       
+//         </div>
+//         <Footer/>
+//         </>)
+// }
+
+// export default Speakers
 
 
 
