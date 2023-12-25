ca351cae2af10765a2bf0b206e1b23a04c6e4e2b
Abhijith236 <aabhijith236@gmail.com>
2023-12-26 00:50:28 +0530

    workshop-content-d

--- frontend/src/components/Workshops.jsx
+++ frontend/src/components/Workshops.jsx
@@ -3,6 +3,14 @@ import { MdArrowOutward } from "react-icons/md";
 import { Link } from "react-router-dom";
 import { Tilt } from "react-tilt";
 import Navbar from "./navbar";
+import img1 from "../assets/Spectro.jpeg"
+import img2 from "../assets/ideaofs.jpeg"
+import img3 from "../assets/ofs.jpeg"
+import img4 from "../assets/viewofs.jpeg"
+import img5 from "../assets/movesofs.jpeg"
+import img6 from "../assets/faceofs.jpeg"
+import img7 from "../assets/starofs.jpeg"
+import img8 from "../assets/voiceofs.jpeg"
 
 const Workshops = () => {
   const post =[{
@@ -10,7 +18,7 @@ const Workshops = () => {
       Event:"Dive Into Spectra",
       Caption:"Don't miss the incredible Technical Quiz Extravaganza ! Explore mind-bending",
       link:'https://www.instagram.com/p/C1Hvwy9SAFJ/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==',
-      img:'SpectroSpect-Main/frontend/src/assets/CTA.png',
+      img: img1,
 
   },
     {
@@ -18,49 +26,49 @@ const Workshops = () => {
       Event:"Idea Pitching",
       Caption:"Unleash your innovation, inspire, and let the sparks fly! Get ready to pitch your brilliance! ",
       link:'',
-      img:"SpectroSpect-Main/frontend/src/assets/ideaofs.jpeg",   
+      img: img2,   
     },
     {
       title:"SpectroSpect",
       Event:"Of Spect Series",
       Caption:"Brace yourselves for a pulse-pounding lineup of electrifying series of events",
       link:'',
-      img:"SpectroSpect-Main/frontend/src/assets/ofs.jpeg",   
+      img: img3,   
     },
     {
       title:"SpectroSpect",
       Event:"View of Spect",
       Caption:"Get ready to unleash your inner explorer, capture the unseen, and reveal the hidden beauty ",
       link:'',
-      img:"SpectroSpect-Main/frontend/src/assets/viewofs.jpeg",   
+      img: img4,   
     },
     {
       title:"SpectroSpect",
       Event:"Moves of Spect",
       Caption:"Step into the virtual spotlight and showcase your acting prowess",
       link:'https://www.instagram.com/p/C1HvqNSSeyN/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==',
-      img:"SpectroSpect-Main/frontend/src/assets/movesofs.jpeg",   
+      img: img5,   
     },
     {
       title:"SpectroSpect",
       Event:"Face of Spect",
       Caption:"Demonstrate your modeling expertise in Face of Spect ",
       link:'',
-       img:"SpectroSpect-Main/frontend/src/assets/faceofs.jpeg",   
+       img: img6,   
     },
     {
       title:"SpectroSpect",
       Event:"Star of Spect",
       Caption:"Step into the virtual spotlight and showcase your acting prowess",
       link:'',
-      img:"SpectroSpect-Main/frontend/src/assets/starofs.jpeg",   
+      img: img7,   
     },
     {
       title:"SpectroSpect",
       Event:"Voice of Spect",
       Caption:"Get ready to unleash your inner explorer, capture the unseen, and reveal the hidden beauty ",
       link:'',
-      img:"SpectroSpect-Main/frontend/src/assets/voiceofs.jpeg",   
+      img: img8,   
     },
   ]
 
