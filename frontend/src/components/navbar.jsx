dd2daa711c3d669197dad26a08fe8a1faa6c611f
DATAPIRATE17 <124865981+DATAPIRATE17@users.noreply.github.com>
2023-12-26 14:01:06 +0530

    Navbar Responsiveness

--- frontend/src/components/navbar.jsx
+++ frontend/src/components/navbar.jsx
@@ -1,61 +1,74 @@
+import React, { useState, useEffect } from 'react';
+import './navbar.css';
+import spectrospectLogo from '../assets/ezgif.gif';
 
-import React, { useState } from 'react';
-import './Navbar.css';
-import spec from "../assets/spect.jpg";
-import { MdFacebook } from "react-icons/md";
-import { FaInstagram } from "react-icons/fa";
-import { FiMail } from "react-icons/fi";
-import MobileMenu from './MobileMenu';
+const Navbar = () => {
+  const [isNavOpen, setIsNavOpen] = useState(false);
+  const [isMobile, setIsMobile] = useState(window.innerWidth <= 768);
+  const [scrolling, setScrolling] = useState(false);
 
-const Navbar = ({ setSticky = false }) => {
-  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 768);
+    };
 
-  const toggleMobileMenu = () => {
-    setMobileMenuOpen(!mobileMenuOpen);
+    const handleScroll = () => {
+      setScrolling(window.scrollY > 0);
+    };
+
+    window.addEventListener('resize', handleResize);
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+  const toggleNav = () => {
+    setIsNavOpen(!isNavOpen);
   };
 
-  const closeMobileMenu = () => {
-    setMobileMenuOpen(false);
+  const closeNav = () => {
+    setIsNavOpen(false);
+  };
+
+  const handleNavItemClick = () => {
+    closeNav();
   };
 
   return (
-    <div className={`navBarContainer ${mobileMenuOpen ? 'mobile-menu-open' : ''}`}>
-      <div className='imgg'>
-        <img src={spec} alt="" />
-      </div>
-      <nav style={setSticky ? { position: "fixed" } : null}>
-        <div className='hamburger-menu' onClick={toggleMobileMenu}>
-          <div className='bar'></div>
-          <div className='bar'></div>
-          <div className='bar'></div>
-        </div>
-        {/* Render MobileMenu component conditionally */}
-        {mobileMenuOpen && <MobileMenu closeMobileMenu={closeMobileMenu} />}
-        <ul className={`nav-links ${mobileMenuOpen ? 'mobile-menu-active' : ''}`}>
-          <li><a href="#" className="navv">Home</a></li>
-          <li><a href="#about" className="navv">About</a></li>
-          <li><a href="#Workshops" className="navv">Workshops</a></li>
-          <li><a href="#" className="navv">Contact</a></li>
-        </ul>
-        <div className='header-sponsors'>
-          <div id="footer-slider-1">Our Sponsors</div>
-          <div id="footer-slider-2"></div>
-          <div id="footer-slider-3">
-            <a className="hover:cursor-pointer hover:text-gray-400 transition duration-500 "><MdFacebook /></a>
-            <a className="hover:cursor-pointer hover:text-gray-400 transition duration-500"><FaInstagram /></a>
-            <a className="hover:cursor-pointer hover:text-gray-400 transition duration-500"><FiMail /></a>
-            <a className="hover:cursor-pointer hover:text-gray-400 transition duration-500 "><MdFacebook /></a>
+    <nav className={`navbar ${isMobile && scrolling ? 'fixed' : ''} ${isNavOpen ? 'open' : ''}`}>
+      <div className="burger-menu" onClick={toggleNav}>
+        {isNavOpen ? (
+          <div className="cross-icon">
+            <div className="bar1"></div>
+            <div className="bar2"></div>
           </div>
-        </div>
-        <div className="gif-container">
-          <img
-            className="ezgif"
-            src="src\assets\ezgif.gif"  
-            alt="Ezgif"
-          />
-        </div>
-      </nav>
-    </div>
+        ) : (
+          <>
+            <div className="bar"></div>
+            <div className="bar"></div>
+            <div className="bar"></div>
+          </>
+        )}
+      </div>
+      <div className={`nav-links ${isNavOpen ? 'open' : ''}`}>
+        <a href="#" onClick={handleNavItemClick}>
+          Home
+        </a>
+        <a href="#about" onClick={handleNavItemClick}>
+          About
+        </a>
+        <a href="#Workshops" onClick={handleNavItemClick}>
+          Workshops
+        </a>
+        <a href="#" onClick={handleNavItemClick}>
+          Contact
+        </a>
+      </div>
+      <img className="logo" src={spectrospectLogo} alt="SPECTROSPECT Logo" />
+    </nav>
   );
 };
 
