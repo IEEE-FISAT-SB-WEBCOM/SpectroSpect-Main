7158d1d85c0a0bf175e006a7a94b2e8c39ffc113
Akhil Gireesh <akhilgireeshakg@gmail.com>
2023-12-31 20:59:15 +0530

    wk array

--- frontend/src/components/WorkshopContent.js
+++ frontend/src/components/WorkshopContent.js
@@ -3,6 +3,10 @@ import img2 from '../assets/workshop2.png'
 import img3 from '../assets/workshop3.png'
 import img4 from '../assets/workshop4.png'
 import img5 from '../assets/workshop5.png'
+import Main1 from "../assets/Mathlabwk.png"
+import Main2 from "../assets/Quantumwk.png"
+import Main3 from "../assets/SpeechRecogwk.png"
+import Main4 from "../assets/Blockwk.png"
 
 const WorkshopContent = [
     {
@@ -12,7 +16,8 @@ const WorkshopContent = [
         Perks:"Delve into the realm of Digital Signal Processing (DSP) with MATLAB at our Spectrospect workshop! This hands-on experience promises an exciting journey through the intricacies of digital signals. By participating, you'll not only master the art of signal processing but also gain proficiency in MATLAB—an invaluable skill in the tech world. The workshop offers expert guidance, networking opportunities, and a certification to validate your newfound expertise. Don't miss out on this chance to elevate your DSP skills and enhance your professional portfolio. Join us for a workshop that combines learning with practical application!",
         Speaker:["Revealing soon"],
         SpeakerDesc:"Get ready for an immersive learning experience at Spectrospect! Our four dynamic workshop sessions, curated by industry experts, cover a spectrum of topics, from hands-on technical sessions to insightful discussions on emerging trends. Whether you're a seasoned professional or a budding enthusiast, these workshops on February 16-18 offer a unique opportunity to delve deep into the practical aspects of cutting-edge technologies.",
-        WImg:img1
+        WImg:img1,
+        MImg:Main1
     },
     {
         ID:2,
@@ -21,7 +26,8 @@ const WorkshopContent = [
         Perks:"Embark on a groundbreaking journey at our Quantum Signal Processing workshop during Spectrospect! Immerse yourself in the fusion of science and technology, exploring the transformative realm of quantum computing and its pivotal role in signal processing. By joining this workshop, you'll unlock a myriad of perks, including hands-on exposure to quantum technologies, insights into the future of signal processing, and the opportunity to interact with experts in the field. Seize the chance to be at the forefront of innovation, expanding your skill set and gaining a unique perspective on the convergence of quantum mechanics and signal processing. Elevate your understanding and expertise—register now for an illuminating experience! ",
         Speaker:["Revealing soon"],
         SpeakerDesc:"Get ready for an immersive learning experience at Spectrospect! Our four dynamic workshop sessions, curated by industry experts, cover a spectrum of topics, from hands-on technical sessions to insightful discussions on emerging trends. Whether you're a seasoned professional or a budding enthusiast, these workshops on February 16-18 offer a unique opportunity to delve deep into the practical aspects of cutting-edge technologies.",
-        WImg:img2
+        WImg:img2,
+        MImg:Main2
     },
     {
         ID:3,
@@ -30,7 +36,8 @@ const WorkshopContent = [
         Perks:"Immerse yourself in the fascinating universe of Speech Recognition with our captivating workshop at Spectrospect! By participating, you gain access to a treasure trove of perks designed to enhance your knowledge and skills. Engage in hands-on learning to unravel the secrets of cutting-edge technology, exploring the intricacies of speech-to-text conversion. Whether you're a novice or a seasoned professional, this workshop provides a unique opportunity to grasp the basics and master advanced techniques in speech recognition. Empower yourself to harness the power of spoken language in the digital realm, gaining a competitive edge in the evolving landscape of technology. Elevate your expertise—register now for an enriching experience that promises to redefine your understanding of speech recognition! ",
         Speaker:["Revealing soon"],
         SpeakerDesc:"Get ready for an immersive learning experience at Spectrospect! Our four dynamic workshop sessions, curated by industry experts, cover a spectrum of topics, from hands-on technical sessions to insightful discussions on emerging trends. Whether you're a seasoned professional or a budding enthusiast, these workshops on February 16-18 offer a unique opportunity to delve deep into the practical aspects of cutting-edge technologies.",
-        WImg:img3
+        WImg:img3,
+        MImg:Main3
     },
     {
         ID:4,
@@ -39,7 +46,8 @@ const WorkshopContent = [
         Perks:"Embark on an exhilarating journey at our immersive workshop, where Blockchain technology meets Signal Processing at Spectrospect! By joining, you'll unlock a host of perks designed to enrich your understanding and skills. Dive deep into the intersection of these dynamic fields, gaining insights into how Blockchain enhances signal processing applications. Enjoy hands-on learning, discovering the practical implications and potential of this powerful synergy. Network with industry experts, fostering collaborations in this cutting-edge domain. Elevate your expertise with a workshop that not only explores the convergence of Blockchain and Signal Processing but also equips you with the tools to navigate and innovate within these transformative landscapes. Don't miss this chance to be at the forefront of technology—register now! ",
         Speaker:["Revealing soon"],
         SpeakerDesc:"Get ready for an immersive learning experience at Spectrospect! Our four dynamic workshop sessions, curated by industry experts, cover a spectrum of topics, from hands-on technical sessions to insightful discussions on emerging trends. Whether you're a seasoned professional or a budding enthusiast, these workshops on February 16-18 offer a unique opportunity to delve deep into the practical aspects of cutting-edge technologies.",
-        WImg:img4
+        WImg:img4,
+        MImg:Main4
     },
    
 ]
