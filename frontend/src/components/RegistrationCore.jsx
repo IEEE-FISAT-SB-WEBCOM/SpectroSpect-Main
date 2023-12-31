b5fc7f13db8dcbbfcbbb2e8632dd6e04c1845e4c
Akhil Gireesh <akhilgireeshakg@gmail.com>
2023-12-31 15:19:24 +0530

    Registration UI  fixed

--- frontend/src/components/RegistrationCore.jsx
+++ frontend/src/components/RegistrationCore.jsx
@@ -1,5 +1,68 @@
 import { MdArrowOutward } from "react-icons/md";
+import gsap from 'gsap'
+import { ScrollTrigger } from 'gsap/all'
+import { useEffect } from "react";
+gsap.registerPlugin(ScrollTrigger)
+
 const RegistrationCore = () => {
+   
+    useEffect(() => {
+        gsap.fromTo(".headingRegistration",{
+            opacity:0,
+            x:-200
+          },{opacity:1,
+            x:0,
+            duration:2,
+            //stagger:0.5,
+            scrollTrigger:{trigger:".headingRegistration",toggleActions:"restart none none none"}
+          })
+
+          gsap.fromTo("#registrationContainer div",{
+            opacity:0,
+            y:200,
+          },{opacity:1,
+            y:0,
+            duration:2,
+            stagger:0.5,
+            scrollTrigger:{trigger:"#registrationContainer",toggleActions:"restart none none none"}
+          })
+
+          gsap.fromTo(".regsitrationCard h1",{
+            opacity:0,
+            scale:0.5,
+          },{opacity:1,
+            duration:3,
+            delay:1.5,
+            scale:1,
+            stagger:1,
+            scrollTrigger:{trigger:".regsitrationCard",toggleActions:"restart none none none"}
+          })
+
+          gsap.fromTo(".regsitrationCard p",{
+            opacity:0,
+            scale:0.5,
+          },{opacity:1,
+            duration:3,
+            delay:2,
+            scale:1,
+            stagger:1,
+            scrollTrigger:{trigger:".regsitrationCard",toggleActions:"restart none none none"}
+          })
+
+          gsap.fromTo(".regsitrationCard button",{
+            opacity:0,
+            y:30,
+            scale:0.5,
+          },{opacity:1,
+            duration:3,
+            y:0,
+            delay:2,
+            scale:1,
+            stagger:1,
+            scrollTrigger:{trigger:".regsitrationCard",toggleActions:"restart none none none"}
+          })
+    })
+    
     return(
         <div>
             <br/>
@@ -11,13 +74,13 @@ const RegistrationCore = () => {
                 <MdArrowOutward style={{marginLeft:window.innerHeight>window.innerWidth?"10vw":"5vw",color:"#33FFA9",fontSize:"45px",marginRight:"0.5vw"}}/>
                 <h1 style={{fontWeight:"700"}}>Registration</h1>
             </div>
-            <div style={{display:"flex",justifyContent:"space-around",alignItems:"center",flexDirection:window.innerHeight>window.innerWidth?"column":"row"}}>
+            <div id="registrationContainer" style={{display:"flex",justifyContent:"space-around",alignItems:"center",flexDirection:window.innerHeight>window.innerWidth?"column":"row"}}>
                 <div className="regsitrationCard" style={{width:window.innerHeight>window.innerWidth?"80vw":"30vw"}}>
                     <h1>IEEE SPS MEMBERS</h1>
-                    <p>Food and Accommodation<br/>(3 days and 2 nights)<br/>
-                        4x Workshop<br/>
-                        3x Talk Session<br/>
-                        2x Culturals<br/>
+                    <p>Food and Accommodation<br/>(3 days and 2 nights)<br/><br/>
+                        <b>4x</b> Workshop<br/>
+                        <b>3x</b> Talk Session<br/>
+                        <b>2x</b> Culturals<br/><br/>
                         Exciting Games<br/>
                         Goodies
                     </p>
@@ -26,10 +89,10 @@ const RegistrationCore = () => {
                 </div>
                 <div className="regsitrationCard" style={{width:window.innerHeight>window.innerWidth?"80vw":"30vw"}}>
                     <h1>IEEE MEMBERS</h1>
-                    <p>Food and Accommodation<br/>(3 days and 2 nights)<br/>
-                        4x Workshop<br/>
-                        3x Talk Session<br/>
-                        2x Culturals<br/>
+                    <p>Food and Accommodation<br/>(3 days and 2 nights)<br/><br/>
+                        <b>4x</b> Workshop<br/>
+                        <b>3x</b> Talk Session<br/>
+                        <b>2x</b> Culturals<br/><br/>
                         Exciting Games<br/>
                         Goodies
                     </p>
@@ -39,10 +102,10 @@ const RegistrationCore = () => {
                 </div>
                 <div className="regsitrationCard" style={{width:window.innerHeight>window.innerWidth?"80vw":"30vw"}}>
                     <h1>NON IEEE MEMBERS</h1>
-                    <p>Food and Accommodation<br/>(3 days and 2 nights)<br/>
-                        4x Workshop<br/>
-                        3x Talk Session<br/>
-                        2x Culturals<br/>
+                    <p>Food and Accommodation<br/>(3 days and 2 nights)<br/><br/>
+                        <b>4x</b> Workshop<br/>
+                        <b>3x</b> Talk Session<br/>
+                        <b>2x</b> Culturals<br/><br/>
                         Exciting Games<br/>
                         Goodies
                     </p>
