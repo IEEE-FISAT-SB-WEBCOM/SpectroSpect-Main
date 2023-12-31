503b2727fe8bf4ff6c5104f7d6249c4d9c8ad9af
Akhil Gireesh <akhilgireeshakg@gmail.com>
2023-12-31 20:09:32 +0530

    Firebase

--- frontend/src/components/LandingPage.jsx
+++ frontend/src/components/LandingPage.jsx
@@ -73,19 +73,19 @@ const LandingPage = () => {
       },
     },
   };
-  // const firebaseConfig = {
-  //   apiKey: import.meta.env.VITE_APIKEY,
-  //   authDomain: import.meta.env.VITE_AUTHDOMAIN,
-  //   databaseURL: import.meta.env.VITE_DATABASEURL,
-  //   projectId: import.meta.env.VITE_PROJECTID,
-  //   storageBucket: import.meta.env.VITE_STORAGEBUCKET,
-  //   messagingSenderId: import.meta.env.VITE_MSID,
-  //   appId: import.meta.env.VITE_APPID,
-  //   measurementId: import.meta.env.VITE_MEASUREMENTID
-  // };
-  // firebase.initializeApp(firebaseConfig);
+  const firebaseConfig = {
+    apiKey: import.meta.env.VITE_APIKEY,
+    authDomain: import.meta.env.VITE_AUTHDOMAIN,
+    databaseURL: import.meta.env.VITE_DATABASEURL,
+    projectId: import.meta.env.VITE_PROJECTID,
+    storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+    messagingSenderId: import.meta.env.VITE_MSID,
+    appId: import.meta.env.VITE_APPID,
+    measurementId: import.meta.env.VITE_MEASUREMENTID
+  };
+  firebase.initializeApp(firebaseConfig);
   
-  // const dataref= firebase.database();
+  const dataref= firebase.database();
 
   const [email, setemail] = useState('');
   const handleClick=()=>{
