6be4891fbc926f64a59b9cc3bd37f99f03fd7bc7
Akhil Gireesh <akhilgireeshakg@gmail.com>
2024-01-15 23:39:07 +0530

    Maps

--- frontend/src/components/LandingPage.jsx
+++ frontend/src/components/LandingPage.jsx
@@ -359,6 +359,11 @@ theme="dark"
             </div>
             </div>
             </div>
+            <br/>
+            <br/>
+            <br/>
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3926.3724180250515!2d76.40665101478143!3d10.231517592692693!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3b08068aa17bd247%3A0xf048b9ebcbd2af28!2sFederal%20Institute%20of%20Science%20And%20Technology%20(FISAT)%C2%AE!5e0!3m2!1sen!2sin!4v1678482024451!5m2!1sen!2sin" style={{width:"76vw",marginLeft:"12vw",height:"40vh"}}></iframe>
+
 
   <div className="flex flex-col lg:flex-row bg-black items-center justify-center" id="Workshops">
         <div className="md:w-3/4 flex flex-col  md:mt-32 lg:mr-8 px-3 py-3">
@@ -369,8 +374,6 @@ theme="dark"
              <Workshop2/>
             </div>
             </div>
-
-
 
 
 {/* Speaker Section */}
